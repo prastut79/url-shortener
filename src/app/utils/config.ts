0c3bf4d96ada18c6cb28f89fcb54507709f975b3
Zero <netsos798@gmail.com>
2023-09-26 21:06:54 +0545

    Next Config Ignore linting during build

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,7 +1,7 @@
 export const PROD_PATH =
 	process.env.NODE_ENV === "development"
 		? "http://localhost:3000"
-		: process.env.PROD_PATH ?? "url-shortner.vercel.app";
+		: process.env.PROD_PATH ?? "https://url-shortener.prastut.com.np";
 
 export const API_URL = PROD_PATH + "/api/url";
 
